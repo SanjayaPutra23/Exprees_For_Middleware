c3d84708b0a5dcf8eda22d7b99936c97ab82dddc
Sanjaya_023 <sanjaya20004@gmail.com>
2024-09-14 19:39:40 +0700

    Project Implementasi Middleware dan Menambahkan ErrorHanddler

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 morgan = require('morgan');
 
+const ErrorHandler = require('./ErrorHandler');
+
 app.use(morgan('dev'));
 app.use((req, res, next) => {
     req.timeRequest = Date.now();
@@ -14,7 +16,8 @@ const auth = (req, res, next) => {
     if (password === 'AdaUdang') {
         next();
     }
-    res.send('Perlu masukkan password');
+    // res.send('Perlu masukkan password');
+    throw new ErrorHandler('Perlu masukkan password', 401);
 }
 
 app.get('/', (req, res) => {
@@ -26,13 +29,35 @@ app.get('/halaman', (req, res) => {
     res.send('Hello Halaman!');
 });
 
+app.get('/erorr', (req, res) => {
+    bird.fly();
+});
+
 app.get('/admin', auth, (req, res) => {
     res.send('Hello admin!');
+});
+
+app.get('/general/error', (req, res) => {
+    throw new Errorhandler();
+});
+
+// app.use((err, req, res, next) => {
+//     console.log('****************************');
+//     console.log('***********ERROR************');
+//     console.log('****************************');
+//     next(err);
+// });
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
 });
 
 app.use((res, res) => {
     res.statusCode(404).send('Page Not Found')
 });
+
+
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
